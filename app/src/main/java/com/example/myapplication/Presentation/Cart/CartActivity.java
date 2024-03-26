aa6a9ac3ca1bd4fdf62ed584c726c17af669cc71
MaiHop <maihop456@gmail.com>
2024-03-26 16:59:15 +0700

    update

--- app/src/main/java/com/example/myapplication/Presentation/Cart/CartActivity.java
+++ app/src/main/java/com/example/myapplication/Presentation/Cart/CartActivity.java
@@ -1,26 +1,31 @@
 package com.example.myapplication.Presentation.Cart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
-
-import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
     private RecyclerView rc_ListCart;
     private Button btn_DatHang;
+    private Toolbar toolbar_cart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
         init();
+
+
 
         this.rc_ListCart =findViewById(R.id.rv_ListCart);
 
@@ -31,7 +36,28 @@ public class CartActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_cart, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.item_search_cart){
+            Toast.makeText(this,"OK",Toast.LENGTH_SHORT).show();
+            return true;
+        }else{
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
+
     private void init(){
         this.btn_DatHang = findViewById(R.id.btn_DatHang);
+        this.toolbar_cart = findViewById(R.id.toolbar_cart);
+        setSupportActionBar(toolbar_cart);
+        
     }
 }

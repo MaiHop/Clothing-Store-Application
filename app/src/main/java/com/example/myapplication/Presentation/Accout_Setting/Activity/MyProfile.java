c53bafea911b690cadc4b3a8006328881ce60fc8
hung573 <81184424+hung573@users.noreply.github.com>
2024-05-13 01:59:47 +0700

    update
    Myprofile

--- app/src/main/java/com/example/myapplication/Presentation/Accout_Setting/Activity/MyProfile.java
+++ app/src/main/java/com/example/myapplication/Presentation/Accout_Setting/Activity/MyProfile.java
@@ -4,42 +4,76 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.myapplication.Presentation.LoginAccout.HomeThamGia;
+import com.example.myapplication.Presentation.LoginAccout.SingUp.sign_up;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyProfile extends AppCompatActivity {
     ImageView img_Avata,img_pack_myprofile;
-    TextView txtip_fullname,txtip_email;
+    TextView txtip_fullname,txtip_email,txtip_phone;
 
     Spinner spinnerGender;
     EditText DP_Data;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser mUser = mAuth.getCurrentUser();
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    Uri mImageUri;
+
+    Button btn_image;
+
+
+
+    Button btn_UPdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.c_hung_activity_my_profile);
         init();
 
-        Picasso.get().load(mUser.getPhotoUrl().toString()).into(img_Avata);
-        txtip_fullname.setText(mUser.getDisplayName());
-        txtip_email.setText(mUser.getEmail());
+        loadUserData();
         spinnerGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -65,8 +99,33 @@ public class MyProfile extends AppCompatActivity {
             }
         });
 
+        btn_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+        btnUpdate();
+
 
     }
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(img_Avata);
+        }
+    }
+
 
     private void showDatePickerDialog() {
         final Calendar calendar = Calendar.getInstance();
@@ -96,20 +155,197 @@ public class MyProfile extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerGender.setAdapter(adapter);
         DP_Data = findViewById(R.id.DP_Data);
-
+        btn_UPdate = findViewById(R.id.btn_saveUpdate);
+        txtip_phone = findViewById(R.id.txtip_phone);
+        btn_image = findViewById(R.id.btn_image);
     }
-    public void updateProfile(String newName, Uri newPhotoUri) {
+
+    public void updateProfile(String newName) {
 
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                 .setDisplayName(newName)
-                .setPhotoUri(newPhotoUri)
+//                .setPhotoUri(newPhotoUri)
                 .build();
 
         mUser.updateProfile(profileUpdates)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("name", mUser.getDisplayName());
+//                        map.put("profile", user.getPhotoUrl().toString());
+
+                        database.collection("users").document(mUser.getUid())
+                                .set(map, SetOptions.merge())
+                                .addOnSuccessListener(aVoid -> {
+                                    Log.d("Firestore", "DocumentSnapshot successfully written!");
+                                })
+                                .addOnFailureListener(e -> {
+                                    Log.e("Firebase", "Failed to write user to database", e);
+                                });
                     }
                 });
     }
+    private void showDialogSuccess() {
+        final Dialog successDialog = new Dialog(MyProfile.this);
+        successDialog.setContentView(R.layout.cancel_success);
+
+        // Đặt các thuộc tính cho window của dialog mới, giống như bạn đã làm cho dialog hủy đơn
+        Window window = successDialog.getWindow();
+        if (window != null) {
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            window.setGravity(Gravity.BOTTOM);
+            window.getAttributes().windowAnimations = R.style.DialogAnimation;
+        }
+
+        successDialog.show();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                successDialog.dismiss();
+            }
+        }, 2000); // Đóng dialog sau 2 giây
+
+    }
+    private void showDialogFailed() {
+        final Dialog failedDialog = new Dialog(MyProfile.this);
+        failedDialog.setContentView(R.layout.c_hung_update_false);
+
+        // Đặt các thuộc tính cho window của dialog mới, giống như bạn đã làm cho dialog hủy đơn
+        Window window = failedDialog.getWindow();
+        if (window != null) {
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            window.setGravity(Gravity.BOTTOM);
+            window.getAttributes().windowAnimations = R.style.DialogAnimation;
+        }
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                failedDialog.dismiss();
+            }
+        }, 2000); // Đóng dialog sau 2 giây
+
+        failedDialog.show();
+    }
+    public void updateFirestoreDetails(String userId, String phone, String gender, String birthdate) {
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("phone", phone);
+        user.put("gender", gender);
+        user.put("birthdate", birthdate);
+
+        database.collection("users").document(userId)
+                .set(user, SetOptions.merge())
+                .addOnSuccessListener(aVoid -> Log.d("Firestore", "DocumentSnapshot successfully written!"))
+                .addOnFailureListener(e -> Log.w("Firestore", "Error writing document", e));
+    }
+    public void btnUpdate(){
+        btn_UPdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newName = txtip_fullname.getText().toString();
+                String newPhone = txtip_phone.getText().toString();
+                String newGender = spinnerGender.getSelectedItem().toString();
+                String newBirthdate = DP_Data.getText().toString();
+
+                updateProfile(newName);
+                updateFirestoreDetails(mUser.getUid(), newPhone, newGender, newBirthdate);
+                if (mImageUri != null) {
+                    uploadImageToFirebase(mImageUri);
+                }
+            }
+        });
+    }
+    private void loadUserData() {
+        Picasso.get().load(mUser.getPhotoUrl().toString()).into(img_Avata);
+        txtip_fullname.setText(mUser.getDisplayName());
+        txtip_email.setText(mUser.getEmail());
+        txtip_phone.setText(mUser.getPhoneNumber());
+        if (mUser != null) {
+            DocumentReference docRef = database.collection("users").document(mUser.getUid());
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Hiển thị số điện thoại
+                            String phone = document.getString("phone");
+                            if (phone != null) {
+                                txtip_phone.setText(phone);
+                            }
+
+                            // Hiển thị giới tính
+                            String gender = document.getString("gender");
+                            if (gender != null) {
+                                int spinnerPosition = ((ArrayAdapter<String>) spinnerGender.getAdapter()).getPosition(gender);
+                                spinnerGender.setSelection(spinnerPosition);
+                            }
+
+                            // Hiển thị ngày sinh
+                            String birthdate = document.getString("birthdate");
+                            if (birthdate != null) {
+                                DP_Data.setText(birthdate);
+                            }
+                        } else {
+                            Log.d("Firestore", "No such document");
+                        }
+                    } else {
+                        Log.d("Firestore", "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        if (imageUri != null) {
+            StorageReference fileReference = FirebaseStorage.getInstance().getReference("profile")
+                    .child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+
+            fileReference.putFile(imageUri).continueWithTask(task -> {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                return fileReference.getDownloadUrl();
+            }).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    updateFirebaseUserProfile(downloadUri);
+                } else {
+                    showDialogFailed();
+                }
+            });
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void updateFirebaseUserProfile(Uri imageUrl) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setPhotoUri(imageUrl)
+                    .build();
+
+            user.updateProfile(profileUpdates)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Picasso.get().load(imageUrl).into(img_Avata);
+                            Log.d("Firestore", "No such document");
+                        } else {
+                            Log.d("Firestore", "get failed with ", task.getException());
+                        }
+                    });
+        }
+    }
+
+
 }

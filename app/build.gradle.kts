921b2fa4dc50b5b462b9cc10665e1d8f1c36899f
MaiHop <maihop456@gmail.com>
2024-05-13 08:10:17 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,10 @@ dependencies {
 //  circleindicator
     implementation ("me.relex:circleindicator:2.1.6")
 
+    //Retrofit 2
+    implementation ("com.google.code.gson:gson:2.8.5")
+    implementation ("com.squareup.retrofit2:retrofit:2.4.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.4.0")
+
 
 }

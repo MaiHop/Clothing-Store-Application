3efc1ce44f1654d6bf04cc9c92bb833aedbc61b5
MaiHop <maihop456@gmail.com>
2024-05-13 21:02:56 +0700

    Update FirebaseActivity.java

--- app/src/main/java/com/example/myapplication/Presentation/Cart/Activity/FirebaseActivity.java
+++ app/src/main/java/com/example/myapplication/Presentation/Cart/Activity/FirebaseActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplication.Presentation.Cart.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.myapplication.Data.Data_Source.Api.KieuSP_api;
+import com.example.myapplication.Data.Data_Source.Api.Servicebuilder;
 import com.example.myapplication.Domain.Model.KieuSP;
 import com.example.myapplication.R;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FirebaseActivity extends AppCompatActivity {
 
@@ -17,18 +23,24 @@ public class FirebaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mh_activity_firebase);
 
-//        KieuSP kieu = new KieuSP();
-//        kieu.setId("TestRetrofit");
-//        kieu.setTen("TestRetrofitUpdate");
-//        Call<KieuSP> req = KieuSP_api.;
-//        req.enqueue(new Callback<KieuSP>() {
+        KieuSP_api kieuSPService = Servicebuilder.buildService(KieuSP_api.class);
+        Call<List<KieuSP>> callSync = kieuSPService.readallKieuSP();
+
+        try {
+            List<KieuSP> list = callSync.execute().body();
+            Log.d("TestExcute",String.valueOf(list.size()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+//        callSync.enqueue(new Callback<List<KieuSP>>() {
 //            @Override
-//            public void onResponse(Call<KieuSP> call, Response<KieuSP> response) {
-//                Log.d("UpdateR","OK");
+//            public void onResponse(Call<List<KieuSP>> call, Response<List<KieuSP>> response) {
+//                List<KieuSP> list = response.body();
+//                Log.d("TestExcute",String.valueOf(list.size()));
 //            }
 //
 //            @Override
-//            public void onFailure(Call<KieuSP> call, Throwable t) {
+//            public void onFailure(Call<List<KieuSP>> call, Throwable t) {
 //
 //            }
 //        });

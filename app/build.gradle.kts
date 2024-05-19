0087136006c18067f8648982df5d909bbd146b13
MaiHop <maihop456@gmail.com>
2024-05-19 22:15:14 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,6 @@ dependencies {
     implementation("androidx.paging:paging-runtime:3.0.0")
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
     implementation("com.github.bumptech.glide:glide:4.12.0")
-    implementation("com.google.android.material:material:1.4.0")
 
 
 

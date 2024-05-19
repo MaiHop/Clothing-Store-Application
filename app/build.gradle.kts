d227e35121a9b11ee5f76a4f6b1ad3e725672389
MaiHop <maihop456@gmail.com>
2024-05-19 23:16:08 +0700

    com

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
-    implementation("com.android.tools.compose:compose-preview-renderer:0.0.1-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,6 +67,7 @@ dependencies {
     implementation("androidx.paging:paging-runtime:3.0.0")
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
     implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.android.material:material:1.4.0")
 
 
 
--- app/src/main/java/com/example/myapplication/Presentation/Cart/Activity/GioHangActivity.java
+++ app/src/main/java/com/example/myapplication/Presentation/Cart/Activity/GioHangActivity.java
@@ -51,41 +51,41 @@ public class GioHangActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mh_activity_cart);
 
-        init();
-        this.rv_ListCart =findViewById(R.id.rv_ListCart);
-
-        this.rv_ListCart.setLayoutManager(new LinearLayoutManager(this));
-        gioHangVM = new ViewModelProvider(this).get(GioHangVM.class);
-        gioHangVM.getListCartLiveData().observe(this, new Observer<List<DonHangChiTiet>>() {
-            @Override
-            public void onChanged(List<DonHangChiTiet> donHangChiTiets) {
-                if(!donHangChiTiets.isEmpty()){
-                    list= donHangChiTiets;
-                    GioHangAdapter adapter = new GioHangAdapter(GioHangActivity.this, donHangChiTiets, btn_DatHang, toolbar_title, gioHangVM);
-                    rv_ListCart.setAdapter(adapter);
-                }
-            }
-        });
+//        init();
+//        this.rv_ListCart =findViewById(R.id.rv_ListCart);
+//
+//        this.rv_ListCart.setLayoutManager(new LinearLayoutManager(this));
+//        gioHangVM = new ViewModelProvider(this).get(GioHangVM.class);
+//        gioHangVM.getListCartLiveData().observe(this, new Observer<List<DonHangChiTiet>>() {
+//            @Override
+//            public void onChanged(List<DonHangChiTiet> donHangChiTiets) {
+//                if(!donHangChiTiets.isEmpty()){
+//                    list= donHangChiTiets;
+//                    GioHangAdapter adapter = new GioHangAdapter(GioHangActivity.this, donHangChiTiets, btn_DatHang, toolbar_title, gioHangVM);
+//                    rv_ListCart.setAdapter(adapter);
+//                }
+//            }
+//        });
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mh_menu_toolbar_cart, menu);
-        searchView = (SearchView) menu.findItem(R.id.item_search_cart).getActionView();
-        searchView.setQueryHint("Nhập tên sản phẩm");
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-//                gioHangVM.filterCart(newText.toLowerCase());
-                return false;
-            }
-        });
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.mh_menu_toolbar_cart, menu);
+//        searchView = (SearchView) menu.findItem(R.id.item_search_cart).getActionView();
+//        searchView.setQueryHint("Nhập tên sản phẩm");
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+////                gioHangVM.filterCart(newText.toLowerCase());
+//                return false;
+//            }
+//        });
+//        return true;
+//    }
 
 //    @Override
 //    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -97,46 +97,46 @@ public class GioHangActivity extends AppCompatActivity {
 //            return super.onOptionsItemSelected(item);
 //        }
 //    }
-    private void dathang() {
-        DonHang dh =new DonHang();
-        dh.setListDonHangChiTiet(list);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("DonHangs")
-                .add(dh)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(GioHangActivity.this, "OK", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(GioHangActivity.this, "Fail", Toast.LENGTH_SHORT).show();
-                    }
-                });
-//        Intent intent = new Intent(this, ThanhToanActivity.class);
-//        intent.putExtra("DonHang_api", dh);
-//        startActivity(intent);
-    }
-    private void init(){
-        this.toolbar_title = findViewById((R.id.toolbar_cart_title));
-        this.btn_DatHang = findViewById(R.id.btn_DatHang);
-        this.toolbar_cart = findViewById(R.id.toolbar_cart);
-        setSupportActionBar(toolbar_cart);
-        toolbar_cart.setLogo(R.drawable.mh_baseline_check_box_outline_blank_24);
-
-
-
-        this.btn_DatHang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                toolbar_title.setVisibility(View.GONE);
-                dathang();
-            }
-        });
-
-    }
+//    private void dathang() {
+//        DonHang dh =new DonHang();
+//        dh.setListDonHangChiTiet(list);
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        db.collection("DonHangs")
+//                .add(dh)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Toast.makeText(GioHangActivity.this, "OK", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(GioHangActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+////        Intent intent = new Intent(this, ThanhToanActivity.class);
+////        intent.putExtra("DonHang_api", dh);
+////        startActivity(intent);
+//    }
+//    private void init(){
+//        this.toolbar_title = findViewById((R.id.toolbar_cart_title));
+//        this.btn_DatHang = findViewById(R.id.btn_DatHang);
+//        this.toolbar_cart = findViewById(R.id.toolbar_cart);
+//        setSupportActionBar(toolbar_cart);
+//        toolbar_cart.setLogo(R.drawable.mh_baseline_check_box_outline_blank_24);
+//
+//
+//
+//        this.btn_DatHang.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                toolbar_title.setVisibility(View.GONE);
+//                dathang();
+//            }
+//        });
+//
+//    }
 
 
 }
